2f81f1d1af9d28345e09b670f35db3bd43324bc7
Juwell02 <email2justinw@gmail.com>
2022-03-03 22:07:40 -0600

    Should push to the cloud

--- TraffcLightMidTerm/src/TrafficLight.java
+++ TraffcLightMidTerm/src/TrafficLight.java
@@ -5,6 +5,7 @@ public class TrafficLight {
 			
 	System.out.println("Hello World 2"); 
 	System.out.println("Testing my traffic Controller");
+	System.out.println("Should Print to the master");
 		}
 
 	}
