aa197fc9638da9347bbff4f6c9652f20e3771472
Juwell02 <email2justinw@gmail.com>
2022-03-03 22:04:17 -0600

    Test for this repository

---
+++ TraffcLightMidTerm/src/TrafficLight.java
@@ -0,0 +1,10 @@
+
+public class TrafficLight {
+
+		public static void main(String[] args) {
+			
+	System.out.println("Hello World 2"); 
+	System.out.println("Testing my traffic Controller");
+		}
+
+	}
